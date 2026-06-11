ae31940403dea372eec35544ce5c4bed6b8ddb8e
Jade27gomes <js27gomes@gmail.com>
2026-06-11 16:42:48 -0300

    initial commit

---
+++ creditEngine/src/test/java/com/srm/creditEngine/CreditEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srm.creditEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
