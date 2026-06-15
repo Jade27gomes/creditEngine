764f1243f6acf7498376e9539d74548b76c21541
Jade27gomes <js27gomes@gmail.com>
2026-06-15 18:10:35 -0300

    fix: correcao dos packages e remocao dos arquivos .gitkeep

---
+++ backend/src/main/java/com/srm/creditengine/exception/ResourceNotFoundException.java
@@ -0,0 +1,5 @@
+package com.srm.creditengine.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) { super(message); }
+}
